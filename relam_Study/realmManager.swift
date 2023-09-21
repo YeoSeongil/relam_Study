54676016f7e71cb63253ebbbd65686047befff54
YeoSeongil <seongil_yeo@naver.com>
2023-09-21 21:43:08 +0900

    📚 RealmManager 간단하게 설계 및 메소드 구현

---
+++ relam_Study/realmManager.swift
@@ -0,0 +1,72 @@
+//
+//  realmManager.swift
+//  relam_Study
+//
+//  Created by 여성일 on 2023/09/21.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager {
+    let realmInstance = try! Realm()
+    
+    // --- MARK ---
+    /// Define : realm DB create Data
+    ///
+    func createRealmData(model: testModel) {
+        do {
+            try realmInstance.write {
+                realmInstance.add(model)
+            }
+        } catch {
+            debugPrint("Create Err \(error)")
+        }
+    }
+    
+    // --- MARK ---
+    /// Define : realm DB Update Data
+    ///
+    func updateRealmData(completion: () -> Void) {
+        do{
+            try realmInstance.write {
+                completion()
+            }
+        } catch {
+            debugPrint("Update Err \(error)")
+            return
+        }
+    }
+    
+    // --- MARK ---
+    /// Define : realm DB Delete Data
+    func deleteRealmData(model: testModel) {
+        //        do {
+        //            try realmInstance.delete {
+        //                completion
+        //            }
+        //        } catch {
+        //            debugPrint("realm Delete Err \(error)")
+        //        }
+        //        do {
+        //            try realmInstance.delete(model)
+        //        } catch {
+        //            debugPrint("Delete Err \(error)")
+        //            return
+        //        }
+        //    }
+        // TestCode
+        do {
+            try realmInstance.write {
+                realmInstance.add(model)
+            }
+        }catch {
+            debugPrint("Test : realm DB Delete ERR. \(error)")
+            return
+        }
+    }
+    
+    @objc private func realmTestCode() {
+        
+    }
+}
