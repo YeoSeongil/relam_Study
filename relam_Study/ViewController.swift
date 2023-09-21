d376c09d54dc67bea467f0e17636f270bdca2c79
YeoSeongil <seongil_yeo@naver.com>
2023-09-21 21:44:08 +0900

    📚 뷰컨에서 불러오기 테스트

--- relam_Study/ViewController.swift
+++ relam_Study/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import RealmSwift
 
 
 class ViewController: UIViewController {
 
+    let realm_Manager = RealmManager()
+    
     override func viewDidLoad() {
+        setView()
+        let test_model = testModel(name: "test_code_name", age: 25)
+        realm_Manager.createRealmData(model: test_model)
     }
 
-    
-
+    private func setView() {
+        self.view.backgroundColor = .white
+    }
 }
 
